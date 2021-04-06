c4c8382a8d111a25e985dc7c61d6e64965e67d63
Murtaza <Murtaza_taloo@hotmail.com>
2021-04-06 11:25:00 +1000

    added linkedIn and github links

--- src/mock/data.js
+++ src/mock/data.js
@@ -67,23 +67,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://linkedin.com/in/murtazataloo',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/MurtazaTaloo',
     },
   ],
 };
